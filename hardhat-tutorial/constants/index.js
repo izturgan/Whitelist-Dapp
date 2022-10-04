048ea1ff36d20aa752d12814730d31b918a82c9c
Nurgissa Izturgan <nurgiss@MacBook-Pro-Nurgiss.local>
2022-10-04 14:23:55 +0600

    whitelist

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xeE1f690dc3ec3d29FF8fa260c6d74A5a17e65f1e";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
